8b79b2df4a97c59e3c1f6ef71c9f43c4e919ccf3
Hansley <hansleykowlessur1998@gmail.com>
2021-09-10 22:23:39 +0400

    Component breakdown

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
-
+import RecipeList from './RecipeList';
 
 function App() {
   return (
     <>
+    <RecipeList recipes = {sampleRecipes}/>
     </>
   );
 }
+
+const sampleRecipes = [
+  {
+    id: 1,
+    name: 'Plain Chicken',
+    serving: 3,
+    cookTime: '1:45',
+    instructions: '1. Put salt on Chicken \n 2. Put chicken in oven \n3. Eat chicken'
+  },
+  {
+    id: 2,
+    name: 'Plain Fish',
+    serving: 2,
+    cookTime: '1:45',
+    instructions: '1. Put salt on Fish \n 2. Put fish in oven \n3. Eat fish'
+  }
+]
+
 
 export default App;
