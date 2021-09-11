bafda83e7371f05a7bcfce532484d75e81b98c7e
Hansley <hansleykowlessur1998@gmail.com>
2021-09-11 11:45:35 +0400

    Addition of IngredientList.js

--- src/App.js
+++ src/App.js
@@ -15,14 +15,39 @@ const sampleRecipes = [
     name: 'Plain Chicken',
     serving: 3,
     cookTime: '1:45',
-    instructions: '1. Put salt on Chicken \n 2. Put chicken in oven \n3. Eat chicken'
+    instructions: '1. Put salt on Chicken \n 2. Put chicken in oven \n3. Eat chicken',
+    ingredients: [
+      {
+        id: 1,
+        name : 'Chicken', 
+        amount: '2 Pounds'
+      },
+      {
+        id: 2,
+        name : 'Salt', 
+        amount: '2 Tbs'
+      }
+    ]
   },
   {
     id: 2,
     name: 'Plain Fish',
     serving: 2,
     cookTime: '1:45',
-    instructions: '1. Put salt on Fish \n 2. Put fish in oven \n3. Eat fish'
+    instructions: '1. Put salt on Fish \n 2. Put fish in oven \n3. Eat fish',
+    ingredients: [
+      {
+        id: 1,
+        name : 'Fish', 
+        amount: '1 Pounds'
+      },
+      {
+        id: 2,
+        name : 'Salt', 
+        amount: '2 Tbs'
+      }
+    ]
+  
   }
 ]
 
